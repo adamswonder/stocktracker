7c0a8711d2724c7d0614e03d52cb26460f604eff
moringawonder <adams.wonderboy@student.moringaschool.com>
2022-10-07 22:42:28 +0300

    updates dummy data

--- src/components/data.js
+++ src/components/data.js
@@ -1,38 +1,38 @@
 export const stockData = [
     {
         company: "Twitter Inc",
-        ticker: "TWTR",
+        code: "TWTR",
         stockPrice: "22.76 USD",
-        timeElapsed: "5 sec ago"
+        client: "5 sec ago"
     },
     {
         company: "Square Inc",
-        ticker: "SQ",
+        code: "SQ",
         stockPrice: "45.28 USD",
-        timeElapsed: "10 sec ago"
+        client: "10 sec ago"
     },
     {
         company: "Shopify Inc",
-        ticker: "SHOP",
+        code: "SHOP",
         stockPrice: "341.79 USD",
-        timeElapsed: "3 sec ago"
+        client: "3 sec ago"
     },
     {
         company: "Sunrun Inc",
-        ticker: "RUN",
+        code: "RUN",
         stockPrice: "9.87 USD",
-        timeElapsed: "4 sec ago"
+        client: "4 sec ago"
     },
     {
         company: "Adobe Inc",
-        ticker: "ADBE",
+        code: "ADBE",
         stockPrice: "300.99 USD",
-        timeElapsed: "10 sec ago"
+        client: "10 sec ago"
     },
     {
         company: "HubSpot Inc",
-        ticker: "HUBS",
+        code: "HUBS",
         stockPrice: "115.22 USD",
-        timeElapsed: "12 sec ago"
+        client: "12 sec ago"
     }
 ];
