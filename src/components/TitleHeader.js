4cbac31c9132fe4c36f9b572c7d725e07d990759
moringawonder <adams.wonderboy@student.moringaschool.com>
2022-10-07 23:13:39 +0300

    title udate

--- src/components/TitleHeader.js
+++ src/components/TitleHeader.js
@@ -3,7 +3,7 @@ import React from "react";
 export const TitleHeader = () => {
     return (
         <header className="header">
-            <h2 id="h2">The Adams Project</h2>
+            <h2 id="h2">Fuzzy Waddle</h2>
         </header>
     );
 };
